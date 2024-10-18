c0782dfb5d8d0b9d42653d762aa4c6789b0c07fd
sudhanshu khosla <sudhanshukhosla123@gmail.com>
2024-10-18 14:42:51 +0530

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURL = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURL = "mongodb+srv://sudhanshukhosla123:sudhanshu168@cluster0.0cubxwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 const connecToMongo = ()=>{
